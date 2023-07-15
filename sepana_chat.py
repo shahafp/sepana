f872fed95b3fdf4a281ffd6e3d7df4f765ff2b85
shahafpariente <shahaf.pariente@stargo.co>
2023-07-15 23:10:13 +0300

    first init

--- sepana_chat.py
+++ sepana_chat.py
@@ -1,46 +1,20 @@
-"""Python file to serve as the frontend"""
 import streamlit as st
-# from streamlit_chat import message
-
-from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 
+st.title('🦜🔗 Quickstart App')
 
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 
-chain = load_chain()
-
-# From here down is all the StreamLit UI.
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("LangChain Demo")
-
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-
-if "past" not in st.session_state:
-    st.session_state["past"] = []
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
 
 
-def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
-    return input_text
-
-
-user_input = get_text()
-
-if user_input:
-    output = chain.run(input=user_input)
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state["generated"]:
-
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)

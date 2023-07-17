4267a37976d658acc0c9e3968f43007aa45673fb
shahafpariente <shahaf.pariente@stargo.co>
2023-07-17 22:34:02 +0300

    first init

--- sepana_chat.py
+++ sepana_chat.py
@@ -111,8 +111,11 @@ with response_container:
 
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
             message(st.session_state["generated"][i], key=str(i))
+
+    if st.session_state['past']:
+        for i in range(len(st.session_state['past'])):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 # if openai_api_key and Conversation:
 #     # Allow to download as well

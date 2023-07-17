f6dda826f7d1a33e125f624f581f142ec533d8ec
shahafpariente <shahaf.pariente@stargo.co>
2023-07-17 20:55:56 +0300

    first init

--- sepana_chat.py
+++ sepana_chat.py
@@ -21,7 +21,7 @@ if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi!']
 
 # Layout of input/response containers
-input_container = st.chat_input()
+user_input = st.chat_input()
 # input_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
@@ -38,8 +38,8 @@ def get_text():
 
 
 # Applying the user input box
-with input_container:
-    user_input = get_text()
+# with input_container:
+#     user_input = get_text()
 
 
 # Response output
@@ -56,10 +56,7 @@ with response_container:
         st.session_state.past.append(user_input)
         st.session_state.generated.append(response)
 
-
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])):
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
             message(st.session_state["generated"][i], key=str(i))
-
-

624581e2764b4ca9a4a9bfa0fc48b52ea767bf84
shahafpariente <shahaf.pariente@stargo.co>
2023-07-17 22:54:58 +0300

    first init

--- sepana_chat.py
+++ sepana_chat.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain import ConversationChain
+from langchain import ConversationChain, PromptTemplate
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.memory.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
@@ -40,6 +40,33 @@ if openai_api_key.startswith('sk-'):
 
     # Create a ConversationEntityMemory object if not already created
     if 'entity_memory' not in st.session_state:
+        template = """You are assistance that specialized in scenarios generation.
+        You goal is to create 3 different scenarios for a given state
+        for example: Citizen K receives from her friend a phone number of a real estate lawyer.
+        She calls him, introduces herself and provides a brief explanation of what real estate she’s planning on purchasing:
+        A nice apartment in her neighborhood with an initial price quote of $0.5M. "
+        She wants the lawyer to administer a background check and confirm that all the property papers look clean and she can proceed with the price negotiations
+        
+        and the 3 options might be: "
+        1.The lawyer rejects this because the deal is too small and is not worth the hurdle"
+        2.The lawyer says he can take the project for the price of 1.5% of the property value. The expected due diligence period is 1 month. They schedule a face to face meeting for next week."
+        3.The lawyer request that they meet face to face and look at the price quote and some initial property papers"
+        The user will choose one of the options, once the user chose you need to generate 3 more scenarios"
+        In this case the user choose 2 then you can generate the next scenarios"
+        1. They meet at the lawyer’s office and sign an initial standard agreement"
+        2. They meet at a caffe and the lawyer says he is too busy and the expected due diligence will take 2 months instead of one"
+        3. At the day of the meeting, the lawyer apologies and reschedules for next week"
+        you keep generating scenarios as long the user gives you his choice"
+        Pay attention to answer only with the scenarios with no extra information of explanations, "
+        also make sure you are using enumeration over the scenarios and provide only 3 options"
+
+        {chat_history}
+        Human: {human_input}
+        Chatbot:"""
+
+        prompt = PromptTemplate(
+            input_variables=["chat_history", "human_input"], template=template
+        )
         entity_memory = ConversationBufferMemory(llm=llm, k=5)
         # entity_memory.chat_memory.add_ai_message("You are assistance that specialized in scenarios generation")
         # entity_memory.chat_memory.add_ai_message("You goal is to create 3 different scenarios for a given state\n"
@@ -64,8 +91,9 @@ if openai_api_key.startswith('sk-'):
     # Create the ConversationChain object with the specified configuration
     Conversation = ConversationChain(
         llm=llm,
-        # prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-        memory=st.session_state.entity_memory
+        prompt=prompt,
+        memory=st.session_state.entity_memory,
+        ai_prefix="AI Chatbot"
     )
 else:
     st.markdown(''' 
